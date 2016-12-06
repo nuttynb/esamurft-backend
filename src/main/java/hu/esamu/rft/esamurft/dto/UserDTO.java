4dbb3605c514a99cf4706ebb253bc75a2e1f86e5
Balogh István <pistiz7@gmail.com>
2016-12-06 19:44:53 +0100

    Added facebookId and googleId to UserDTO, saving user's password

--- src/main/java/hu/esamu/rft/esamurft/dto/UserDTO.java
+++ src/main/java/hu/esamu/rft/esamurft/dto/UserDTO.java
@@ -7,17 +7,34 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class UserDTO {
 
     @Id
-    private String id;
-
+    private String userId;
+    private String facebookId;
+    private String googleId;
     private String username;
+    private String password;
 
-
-    public String getId() {
-        return id;
+    public String getUserId() {
+        return userId;
     }
 
-    public void setId(String id) {
-        this.id = id;
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getFacebookId() {
+        return facebookId;
+    }
+
+    public void setFacebookId(String facebookId) {
+        this.facebookId = facebookId;
+    }
+
+    public String getGoogleId() {
+        return googleId;
+    }
+
+    public void setGoogleId(String googleId) {
+        this.googleId = googleId;
     }
 
     public String getUsername() {
@@ -28,10 +45,20 @@ public class UserDTO {
         this.username = username;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     @Override
     public String toString() {
         return "UserDTO{" +
-                "id='" + id + '\'' +
+                "userId='" + userId + '\'' +
+                ", facebookId='" + facebookId + '\'' +
+                ", googleId='" + googleId + '\'' +
                 ", username='" + username + '\'' +
                 '}';
     }
--- src/main/java/hu/esamu/rft/esamurft/servlet/RegisterServlet.java
+++ src/main/java/hu/esamu/rft/esamurft/servlet/RegisterServlet.java
@@ -12,19 +12,26 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class RegisterServlet extends InitializedServlet {
+
     @Autowired
-    UserService userService;
+    private UserService userService;
+
     private static Logger LOG = LoggerFactory.getLogger(RegisterServlet.class);
 
     private static final String USERNAME_KEY = "username";
+    private static final String PASSWORD_KEY = "password";
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String username = req.getParameter(USERNAME_KEY);
+        String password = req.getParameter(PASSWORD_KEY);
+        LOG.info(username);
+        LOG.info(password);
         UserDTO user = new UserDTO();
         user.setUsername(username);
+        user.setPassword(password);
         user = userService.save(user);
         LOG.info(user.toString());
-        resp.getWriter().write(user.getId());
+        resp.getWriter().write(user.getUserId());
     }
 }
