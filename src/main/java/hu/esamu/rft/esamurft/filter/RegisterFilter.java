4fe89e233fe51d580c303eee7c93e0e0b2b01bf1
Nagykéri Bence <nuttyka@gmail.com>
2016-12-12 17:10:20 +0100

    Refactoring

--- src/main/java/hu/esamu/rft/esamurft/filter/RegisterFilter.java
+++ src/main/java/hu/esamu/rft/esamurft/filter/RegisterFilter.java
@@ -1,6 +1,6 @@
 package hu.esamu.rft.esamurft.filter;
 
-import hu.esamu.rft.esamurft.protos.EsamuRFTMessages;
+import hu.esamu.rft.esamurft.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import javax.servlet.*;
 import java.io.IOException;
 
-public class MessageFilter implements Filter {
-    private static Logger LOG = LoggerFactory.getLogger(MessageFilter.class);
+public class RegisterFilter implements Filter {
+    @Autowired
+    UserService userService;
+    private static Logger LOG = LoggerFactory.getLogger(RegisterFilter.class);
+    private static final String USERNAME_KEY = "username";
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -18,11 +21,12 @@ public class MessageFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        EsamuRFTMessages.item message = EsamuRFTMessages.item.parseFrom(servletRequest.getInputStream());
-        LOG.info("Processing " + message.getId() + "'s message (IP: " + servletRequest.getRemoteAddr() + ")...");
+
+        String username = servletRequest.getParameter(USERNAME_KEY);
+        LOG.info("Processing " + username + "'s register (IP: " + servletRequest.getRemoteAddr() + ")...");
         filterChain.doFilter(servletRequest, servletResponse);
-        LOG.info("Processing ended, new message:");
-        LOG.info("\n" + message.toString());
+        LOG.info("Processing ended, new user:");
+        LOG.info("\n");
     }
 
     @Override
--- src/main/java/hu/esamu/rft/esamurft/servlet/MessageServlet.java
+++ src/main/java/hu/esamu/rft/esamurft/servlet/MessageServlet.java
@@ -1,9 +1,5 @@
 package hu.esamu.rft.esamurft.servlet;
 
-
-import com.google.protobuf.InvalidProtocolBufferException;
-import hu.esamu.rft.esamurft.protos.EsamuRFTMessages;
-import hu.esamu.rft.esamurft.service.MessageService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,12 +10,11 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class MessageServlet extends InitializedServlet {
-    @Autowired
-    MessageService messageService;
     private static Logger LOG = LoggerFactory.getLogger(MessageServlet.class);
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //TODO Processing the message...
         /*if (!MimeTypeUtils.APPLICATION_OCTET_STREAM.equals(resp.getContentType())) {
             resp.setStatus(HttpServletResponse.SC_NOT_ACCEPTABLE);
             return;
